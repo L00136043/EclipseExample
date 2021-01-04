6379226a56afd9c19d0c2777f77093dc452b46eb
marty <marty@ubuntu>
2021-01-03 18:07:27 -0800

    This commit is an addition of a simple greeting program.

---
+++ JavaPrjGitEclipse/src/clspkg/Greetings.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package clspkg;
+
+/**
+ * @author marty
+ *
+ */
+public class Greetings {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Greetings l00136043");
+	}
+
+}
